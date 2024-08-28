be11ec68b57d2e74b87faa6c4f8e4618cdabd6a2
lprimak <lenny@flowlogix.com>
2024-08-28 01:11:38 -0500

    dded metamodel and graphene

--- archetypes/src/main/resources/archetype-resources/src/test/java/StarterIT.java
+++ archetypes/src/main/resources/archetype-resources/src/test/java/StarterIT.java
@@ -5,7 +5,7 @@ import com.flowlogix.util.ShrinkWrapManipulator;
 import org.eu.ingwar.tools.arquillian.extension.suite.annotations.ArquillianSuiteDeployment;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit5.ArquillianExtension;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,7 +20,7 @@ class StarterIT {
     }
 
     @Deployment
-    static JavaArchive deploy() {
-        return ShrinkWrapManipulator.createDeployment(JavaArchive.class);
+    static WebArchive deploy() {
+        return ShrinkWrapManipulator.createDeployment(WebArchive.class);
     }
 }
